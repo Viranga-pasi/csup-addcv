dd449cbb2661099ea69c3f23fadb3a3be879e4ba
Viranga Jayawaradana <virangapasindu4@gmail.com>
2020-12-03 13:48:17 +0530

    commit by ravindu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Navigation from "./componenets/navigation";
+import Footer from "./componenets/footer";
 import HomePage from "./pages/homePage";
 import Dashboard from "./pages/dashboard";
 import Profile from "./pages/profile";
@@ -20,7 +20,9 @@ function App() {
           <Route path="/profile" exact component={Profile} />
           <Route path="/profile/:id" component={ProfileDetails} />
         </Switch>
+
       </div>
+     
     </Router>
   );
 }
---
+++ src/componenets/footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Navbar, Nav, Button} from 'react-bootstrap'
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import logo from '../images/logo.png';
+
+function Footer() {
+  
+
+  
+  return (
+    <div>
+
+        <Navbar bg="dark" variant="dark" expand="lg" fixed="bottom">
+            <Navbar.Link>
+                
+                CSUP WEB
+            </Navbar.Link>
+            
+        </Navbar>
+        
+  </div>
+  );
+}
+export default Footer;
