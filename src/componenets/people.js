e033b853e119578cfa98fc89b105cf7a167a7b7d
Viranga Jayawaradana <virangapasindu4@gmail.com>
2020-12-06 13:57:11 +0530

    add layering

--- src/componenets/people.js
+++ src/componenets/people.js
@@ -3,16 +3,18 @@ import React from 'react';
 import Card from 'react-bootstrap/Card'
 import '../App.css';
 import {Link} from 'react-router-dom';
-
+import Image from 'react-bootstrap/Image'
 function People(props) {
   
   
   return (
     <div >
       <Card bsPrefix="people" border="light">
-            <Card.Img variant="top" src={props.img} />
+            {/* <Card.Img variant="top" src={props.img} /> */}
+            <Image src={props.img} roundedCircle style={{width: 175, height: 'auto', margin:5}}/>
             <Card.Body>
-                <Card.Title>{props.name}</Card.Title>
+                <h6>{props.name}</h6>
+                
                 
                 <button className="button">
                     <Link to={props.link} className="link">
