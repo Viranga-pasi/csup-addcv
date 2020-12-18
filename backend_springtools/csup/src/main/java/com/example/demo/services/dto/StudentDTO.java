6dfb7e84dd2b6903b036c1a0ef59e5948e35881a
RavinduFernando <f.ravindu@gmail.com>
2020-12-18 17:09:16 +0530

    added the neccessary packages

--- backend_springtools/csup/src/main/java/com/example/demo/services/dto/StudentDTO.java
+++ backend_springtools/csup/src/main/java/com/example/demo/services/dto/StudentDTO.java
@@ -2,4 +2,39 @@ package com.example.demo.services.dto;
 
 public class StudentDTO {
 
+	
+	private String name;
+	private String address;
+	
+	
+	public StudentDTO(String name, String address) {
+		this.name = name;
+		this.address = address;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+	
+	
+	
 }
